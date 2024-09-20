1ed16833e986253e5864e7c174f3b9d1d5cc1dcc
archops <archopsemail@gmail.com>
2024-09-20 23:47:57 +0530

    updated variables

--- Terraform_Azure_Infra/variable.tf
+++ Terraform_Azure_Infra/variable.tf
@@ -1,6 +1,6 @@
 variable "subscriptionId" {             # During Terraform plan we can input the subscription.
     type = string
-    default = "8d3ed687-4dc7-4a9b-903a-b9a14bde85c4"
+    default = ""
 }
 
 variable "Location" {
